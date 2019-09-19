956946c5861ae7ff0eb495dd8701c2770e694c14
Pablo Rodriguez Nava <info@pablintino.com>
2019-09-20 00:38:31 +0200

    + Added calche vars declarations first

--- toolchain/variables.cmake
+++ toolchain/variables.cmake
@@ -4,8 +4,14 @@ set_property(
   PROPERTY STRINGS
   "Debug" "Release"
 )
-
+set(STARTUP_FILE "" CACHE FILEPATH "Startup file path")
 set(LINKER_FILE "" CACHE FILEPATH  "LD Linker file path")
+# Optional -D compiler definitions
+set(OPTIONAL_DEFINITIONS "" CACHE STRING "Optional compiler definitions")
+# Optional toolchain path
+set(ARM_TOOLCHAIN_DIR "" CACHE STRING "Optional ARM GCC toolchain root path")
+
+
 if(NOT LINKER_FILE OR LINKER_FILE STREQUAL "")
   message(FATAL_ERROR "LINKER_FILE variable not defined. Provide a valid linker file path")
 elseif(NOT IS_ABSOLUTE ${LINKER_FILE})
@@ -15,7 +21,7 @@ if(NOT EXISTS "${LINKER_FILE}")
     message(FATAL_ERROR "LINKER_FILE path does not exist")
 endif()
 
-set(STARTUP_FILE "" CACHE FILEPATH "Startup file path")
+
 if(NOT STARTUP_FILE OR STARTUP_FILE STREQUAL "")
   message(FATAL_ERROR "STARTUP_FILE variable not defined. Provide a valid startup file path")
 elseif(NOT IS_ABSOLUTE ${STARTUP_FILE})
@@ -26,8 +32,3 @@ if(NOT EXISTS "${STARTUP_FILE}")
 endif()
 
 
-# Optional -D compiler definitions
-set(OPTIONAL_DEFINITIONS "" CACHE STRING "Optional compiler definitions")
-
-# Optional toolchain path
-set(ARM_TOOLCHAIN_DIR "" CACHE STRING "Optional ARM GCC toolchain root path")
